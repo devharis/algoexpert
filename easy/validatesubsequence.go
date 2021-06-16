b8334ffc9457067794260ff677be37d9d2ebac49
Haris Kljajic <devhariis@gmail.com>
2021-06-16 19:34:17 +0200

    impl valid subsequence

--- easy/validatesubsequence.go
+++ easy/validatesubsequence.go
@@ -4,10 +4,19 @@ package easy
 // Link: https://www.algoexpert.io/questions/Validate%20Subsequence
 //
 //
-// Time complexity ?
-// Space complexity ?
+// Time complexity O(n)
+// Space complexity O(1)
 
 func IsValidSubsequence(array []int, sequence []int) bool {
-	// Write your code here.
-	return false
+	seqIdx, arrIdx := 0, 0
+	n := len(array)
+	m := len(sequence)
+
+	for arrIdx < n && seqIdx < m {
+		if array[arrIdx] == sequence[seqIdx] {
+			seqIdx++
+		}
+		arrIdx++
+	}
+	return m == seqIdx
 }
